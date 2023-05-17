1d646e918acb03ce5e4f60fcf7ce02f3297b8a6a
Lucas Siqueira <siqueiraq@gmail.com>
2023-05-17 11:35:26 -0300

    alter database

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# crud_flask
+# Crud with Flask
+###To execute the project
+
+Install depedencies with<br>
+<code>pip3 install -r requirements.txt</code>
+
+Run migrations<br>
+<code>flask db init</code><br>
+<code>flask db migrate -m "Starting database"</code><br>
+<code>flask db upgrade</code><br>
+
+Execute the project<br>
+<code>python3 main.py</code>
---
+++ app.py
@@ -0,0 +1,23 @@
+https://www.youtube.com/watch?v=XPPYPb3ByiM
+from flask import Flask
+from database import db
+from flask_migrate import Migrate
+from users import bp_users
+
+app = Flask(__name__)
+
+db.init_app(app)
+connection = "sqlite:///database.sqlite"
+
+app.config["SECRET_KEY"] = "my-key"
+app.config["SQLALCHEMY_DATABASE_URI"] = connection
+app.config["SQLALCHEMY_TRACKMODIFICATIONS"] = False
+app.register_blueprint(bp_users, url_prefix="/users")
+
+migrate = Migrate(app, db)
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+app.run(host="0.0.0.0", port=81, debug=True)
